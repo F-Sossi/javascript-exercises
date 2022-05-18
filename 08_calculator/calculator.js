0d5bf68ffde8e09a2b0b110ad79d462f2853a3e2
Frank Sossi <frank.sossi@gmail.com>
2022-05-18 14:09:39 -0700

    Completed 08_calcualtor all tests pass

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,54 @@
-const add = function() {
+const add = function(a,b) {
+  return Number(a) + Number(b);
+};
+
+const subtract = function(a,b) {
+	return Number(a) - Number(b);
+};
+
+const sum = function(arr) {
+  let temp = 0;
+	for (let i = 0; i < arr.length; i++){
+    temp += arr[i];
+  }
+  return temp;
+};
+
+const multiply = function(arr) {
+
+  if(arr){
+    let temp = 1;
+    for(let i = 0; i < arr.length; i++){
+      temp *= arr[i];
+    }
+
+    return temp;
+
+  }else{
+    return;
+  }
+
+};
+
+const power = function(a,b) {
+  let num = Number(a);
+  let pwr = Number(b);
+  let temp = 1;
+
+  for(let i = 0; i < pwr; i++){
+    temp = temp * num;
+  }
+
+  return temp;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (Number(n) === 0){
+    return 1;
+  }else{
+    return Number(n) * factorial(n - 1)
+  }
 };
 
 // Do not edit below this line
