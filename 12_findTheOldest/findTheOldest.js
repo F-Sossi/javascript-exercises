c11cd20440acd31ce4241f9b0c1a6ad0d558b0ef
Frank Sossi <frank.sossi@gmail.com>
2022-05-24 15:22:02 -0700

    Completed exercise all tests passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const getAge = function(birth, death){
+    if(!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
-};
+const findTheOldest = function(people) {
+
+    const eldest =  people.reduce((oldest, current)=>{
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        return oldestAge > currentAge ? oldest:current;
+    });
+
+    console.log(people);
+    console.log(eldest);
+
+    return eldest;
+
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
