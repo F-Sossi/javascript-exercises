c079c9bfdd46fae332fabce7c4d8ccf56a8a927f
Frank Sossi <frank.sossi@gmail.com>
2022-05-23 21:45:28 -0700

    Completed exercise all tests passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str){ 
+    //              remove spaces                      remove all punctuation
+    const clean = str.replace(/\s+/g, '').toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    console.log(clean);
 
+    let right = clean.length - 1;
+    let left = 0
+
+    while(left < right){
+        if(clean[left] != clean[right]){
+            return false;
+        }
+
+        right--;
+        left++;
+    }
+    return true;
 };
 
 // Do not edit below this line
